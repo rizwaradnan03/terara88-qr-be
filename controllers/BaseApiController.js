93040c9416ec286fec7a0c7b018acbf9f58d4f8f
Rizwar <rizwar.adnanbyantara@gmail.com>
2023-07-17 19:22:08 +0700

    .

--- controllers/BaseApiController.js
+++ controllers/BaseApiController.js
@@ -240,26 +240,26 @@ const confirmSelesai = async (req, res) => {
 
         const formattedDate = `${year}-${month}-${day}`;
 
-        // const datas = {
-        //     'doctype': 'POS Invoice',
-        //     'naming_series': formattedDate.replaceAll('-', '') + '-',
-        //     'customer': 'Dine In',
-        //     'posting_date': formattedDate,
-        //     'company': 'Teras Nusantara 88',
-        //     'items': dataErp,
-        //     'payments': [{
-        //         'mode_of_payment': searchCustomer.jenisPembayaran,
-        //         'amount': parseInt(searchCustomer.totalBayar),
-        //         'type': searchCustomer.jenisPembayaran == 'Cash' ? 'Cash' : 'Bank' 
-        //     }],
-        //     'status': 'Paid'
-        // }
+        const datas = {
+            'doctype': 'POS Invoice',
+            'naming_series': formattedDate.replaceAll('-', '') + '-',
+            'customer': 'Dine In',
+            'posting_date': formattedDate,
+            'company': 'Teras Nusantara 88',
+            'items': dataErp,
+            'payments': [{
+                'mode_of_payment': searchCustomer.jenisPembayaran,
+                'amount': parseInt(searchCustomer.totalBayar),
+                'type': searchCustomer.jenisPembayaran == 'Cash' ? 'Cash' : 'Bank' 
+            }],
+            'status': 'Paid'
+        }
         
-        // const hitApi = axios.post(`${baseUrl}/resource/POS%20Invoice`,{
-        //     data: datas
-        // } ,{
-        //     headers: headerCashier
-        // })
+        const hitApi = axios.post(`${baseUrl}/resource/POS%20Invoice`,{
+            data: datas
+        } ,{
+            headers: headerCashier
+        })
         
         // await axios.post(`${baseUrl}/resource/POS%20Invoice/${hitApi.id}`,{
         //     data: {
